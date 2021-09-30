fd2ee6657d7f6f66d2c6313f51826bde6cb69965
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2021-09-29 22:14:24 -0400

    update

--- mainPageGame.js
+++ mainPageGame.js
@@ -6,9 +6,16 @@ var ctx = canvas.getContext("2d");
 // ctx.fillStyle = "#FF0000";
 // ctx.fill();
 // ctx.closePath();
-add_linkedin();
-add_github();
-add_resume();
+
+var mario_x = 0;
+var mario_y = 250
+var rightPressed = false;
+var leftPressed = false;
+var spacePressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 
 
 function add_linkedin() { 
@@ -18,6 +25,32 @@ function add_linkedin() {
         ctx.drawImage(linkedin,75,50,50,50);
     }
     
+}
+
+
+function keyDownHandler(e) {
+    if(e.code  == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.code == 'ArrowLeft') {
+        leftPressed = true;
+    }
+    else if (e.code == 'Space')
+    {
+        spacePressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.code == 'ArrowRight') {
+        rightPressed = false;
+    }
+    else if(e.code == 'ArrowLeft') {
+        leftPressed = false;
+    }
+    else if (e.code == 'Space')
+    {
+        spacePressed = false;
+    }
 }
 
 function add_github() { 
@@ -34,3 +67,38 @@ function add_github() {
             ctx.drawImage(resume,350,50,40,55);
         }
 }
+
+function add_mario(x, y) { 
+    var mario1 = new Image(); 
+    mario1.src = "Images/mario1.png"; 
+
+    var mario2 = new Image(); 
+    mario2.src = "Images/mario2.png"; 
+    mario1.onload = function (){
+        ctx.drawImage(mario1,mario_x,mario_y,40,55);
+    }
+}
+
+function draw ()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    add_linkedin();
+    add_github();
+    add_resume();
+    add_mario();
+
+    if(rightPressed && mario_x < canvas.width-20) {
+        mario_x += 7;
+    }
+    else if(leftPressed && mario_x > 0) {
+        mario_x -= 7;
+    }
+    else if(spacePressed && mario_x > 0) {
+        mario_y -= 50;
+    }
+
+ 
+    
+}
+
+setInterval(draw,100);
