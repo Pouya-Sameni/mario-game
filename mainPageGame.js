a3e4ea165ad0afb130f5e495af02b7f6ae49a63c
Sameni-Pouya-216491623 <46384821+pouyasameni@users.noreply.github.com>
2021-09-29 21:20:41 -0400

    update

--- mainPageGame.js
+++ mainPageGame.js
@@ -6,13 +6,31 @@ var ctx = canvas.getContext("2d");
 // ctx.fillStyle = "#FF0000";
 // ctx.fill();
 // ctx.closePath();
-add_img();
+add_linkedin();
+add_github();
+add_resume();
 
-function add_img() { 
-    var img = new Image(); 
-    img.src = "Images/Linkedin.png"; 
-    img.onload = function (){
-        ctx.drawImage(img,75,50,50,50);
+
+function add_linkedin() { 
+    var linkedin = new Image(); 
+    linkedin.src = "Images/Linkedin.png"; 
+    linkedin.onload = function (){
+        ctx.drawImage(linkedin,75,50,50,50);
     }
     
+}
+
+function add_github() { 
+    var github = new Image(); 
+    github.src = "Images/github.png"; 
+    github.onload = function (){
+        ctx.drawImage(github,200,50,50,50);
+    }
+}
+    function add_resume() { 
+        var resume = new Image(); 
+        resume.src = "Images/resume_logo.png"; 
+        resume.onload = function (){
+            ctx.drawImage(resume,350,50,40,55);
+        }
 }
