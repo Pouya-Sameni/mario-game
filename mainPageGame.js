90a8b918c833ec2328d6ef03c7e9dbfed9cb6291
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2021-09-30 23:58:04 -0400

    update

--- mainPageGame.js
+++ mainPageGame.js
@@ -8,40 +8,61 @@ var ctx = canvas.getContext("2d");
 // ctx.closePath();
 
 var mario_x = 0;
-var mario_y = 250
+var mario_y = 218;
+var block_x = 70;
+var block_y = 100;
+
 var rightPressed = false;
 var leftPressed = false;
 var spacePressed = false;
 var draw_mario1 = true;
+var draw_mario1_flip = true;
+var draw_mario2 = false;
+var draw_mario2_flip = false;
 
 //Images - OPEN
-var linkedin = new Image(); 
-linkedin.src = "Images/Linkedin.png"; 
-var github = new Image(); 
-github.src = "Images/github.png"; 
-var resume = new Image(); 
-resume.src = "Images/resume_logo.png"; 
-var mario1 = new Image(); 
-mario1.src = "Images/mario1.png"; 
-var mario2 = new Image(); 
-mario2.src = "Images/mario2.png"; 
+var linkedin = new Image();
+linkedin.src = "Images/Linkedin.png";
+var github = new Image();
+github.src = "Images/github.png";
+var resume = new Image();
+resume.src = "Images/resume_logo.png";
+var mario1 = new Image();
+mario1.src = "Images/mario1.png";
+var mario2 = new Image();
+mario2.src = "Images/mario2.png";
+var mario1_flip = new Image();
+mario1_flip.src = "Images/mario1_flip.png";
+var mario2_flip = new Image();
+mario2_flip.src = "Images/mario2_flip.png";
+var mario_back = new Image();
+mario_back.src = "Images/mario_back.png";
 //////////////////////////////
 
 //Load Images
-github.onload = function (){
-    ctx.drawImage(github,200,50,50,50);
+github.onload = function () {
+    ctx.drawImage(github, block_x, block_y, 50, 50);
 }
-linkedin.onload = function (){
-    ctx.drawImage(linkedin,75,50,50,50);
+linkedin.onload = function () {
+    ctx.drawImage(linkedin, block_x+100, block_y, 50, 50);
 }
-resume.onload = function (){
-    ctx.drawImage(resume,350,50,40,55);
+resume.onload = function () {
+    ctx.drawImage(resume, block_x+200, block_y, 40, 55);
 }
-mario1.onload = function (){
-    ctx.drawImage(mario1,mario_x,mario_y,40,55);
+mario1.onload = function () {
+    ctx.drawImage(mario1, mario_x, mario_y, 40, 55);
 }
-mario2.onload = function (){
-    ctx.drawImage(mario2,mario_x,mario_y,40,55);
+mario2.onload = function () {
+    ctx.drawImage(mario2, mario_x, mario_y, 40, 55);
+}
+mario1_flip.onload = function () {
+    ctx.drawImage(mario1_flip, mario_x, mario_y, 40, 55);
+}
+mario2_flip.onload = function () {
+    ctx.drawImage(mario2_flip, mario_x, mario_y, 40, 55);
+}
+mario_back.onload = function () {
+    ctx.drawImage(mario_back, mario_x, mario_y, 40, 55);
 }
 
 
@@ -50,92 +71,135 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-    if(e.code  == "ArrowRight") {
+    if (e.code == "ArrowRight") {
         rightPressed = true;
     }
-    else if(e.code == 'ArrowLeft') {
+    else if (e.code == 'ArrowLeft') {
         leftPressed = true;
     }
-    else if (e.code == 'Space')
-    {
+    else if (e.code == 'Space') {
         spacePressed = true;
     }
 }
 function keyUpHandler(e) {
-    if(e.code == 'ArrowRight') {
+    if (e.code == 'ArrowRight') {
         rightPressed = false;
     }
-    else if(e.code == 'ArrowLeft') {
+    else if (e.code == 'ArrowLeft') {
         leftPressed = false;
     }
-    else if (e.code == 'Space')
-    {
+    else if (e.code == 'Space') {
         spacePressed = false;
     }
 }
 
-function redraw_All() { 
- 
-        ctx.drawImage(linkedin,75,50,50,50);
-        ctx.drawImage(github,200,50,50,50);
-        ctx.drawImage(resume,350,50,40,55);
+function redraw_All() {
+
+    ctx.drawImage(mario_back, 0, 0, 480, 320);
+    ctx.drawImage(github, block_x, block_y, 50, 50);
+    ctx.drawImage(linkedin, block_x+150, block_y, 50, 50);
+    ctx.drawImage(resume, block_x+300, block_y, 40, 55);
+    
 }
 
 
 
-function add_mario() { 
-    
-    
+function add_mario() {
 
-    if (draw_mario1 === true){
-        ctx.drawImage(mario1,mario_x,mario_y,40,55);
-        
+
+
+    if (draw_mario1) {
+
+        ctx.drawImage(mario1, mario_x, mario_y, 40, 55);
     }
-    else{
-        ctx.drawImage(mario2,mario_x,mario_y,40,55);
-       
+    else if (draw_mario1_flip) {
+        ctx.drawImage(mario1_flip, mario_x, mario_y, 40, 55);
+    }
+    else if (draw_mario2) {
+        ctx.drawImage(mario2, mario_x, mario_y, 40, 55);
+    }
+    else if (draw_mario2_flip) {
+        ctx.drawImage(mario2_flip, mario_x, mario_y, 40, 55);
     }
 
-    
-    if (rightPressed || leftPressed)
-    {
-        if (x <= 10)
 
-        {
+    if (rightPressed) {
+        draw_mario1_flip = false;
+        draw_mario2_flip = false;
+        if (x <= 20) {
             x++;
         }
-        else{
+        else {
             console.log(x);
             x = 0;
             draw_mario1 = !draw_mario1;
-            
+            draw_mario2 = !draw_mario2;
+
+            if (draw_mario1) {
+                draw_mario2 = false;
+            }
+            else {
+                draw_mario2 = true;
+            }
+
         }
     }
-   
-    
-    
+    else if (leftPressed) {
+        draw_mario1 = false;
+        draw_mario2 = false;
+        if (x <= 20) {
+            x++;
+        }
+        else {
+            console.log(x);
+            x = 0;
+            draw_mario1_flip = !draw_mario1_flip;
+            draw_mario2_flip = !draw_mario2_flip;
+
+            if (draw_mario1_flip) {
+                draw_mario2_flip = false;
+            }
+            else {
+                draw_mario2_flip = true;
+            }
+
+
+        }
+    }
+    else {
+        if (draw_mario2_flip) {
+            draw_mario2_flip = false;
+            draw_mario1_flip = true;
+        }
+        else if (draw_mario2) {
+            draw_mario2 = false;
+            draw_mario1 = true;
+        }
+
+
+    }
+
 }
 
-function draw ()
-{
-    
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+function draw() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     redraw_All();
     add_mario();
 
-    if(rightPressed && mario_x < canvas.width-40) {
-        mario_x += 7;
+    if (rightPressed && mario_x < canvas.width - 40) {
+        mario_x += 2;
     }
-    else if(leftPressed && mario_x > 0) {
-        mario_x -= 7;
+    else if (leftPressed && mario_x > 0) {
+        mario_x -= 2;
     }
-    else if(spacePressed && mario_x > 0) {
+    else if (spacePressed && mario_x > 0) {
         mario_y -= 50;
     }
 
-    
+
     requestAnimationFrame(draw);
-    
+
 
 }
 
