a5300bbd32a774d61fcc5a5384dce3ede9f3bd51
Sameni-Pouya-216491623 <46384821+pouyasameni@users.noreply.github.com>
2021-09-30 22:12:39 -0400

    update

--- mainPageGame.js
+++ mainPageGame.js
@@ -12,21 +12,42 @@ var mario_y = 250
 var rightPressed = false;
 var leftPressed = false;
 var spacePressed = false;
+var draw_mario1 = true;
+
+//Images - OPEN
+var linkedin = new Image(); 
+linkedin.src = "Images/Linkedin.png"; 
+var github = new Image(); 
+github.src = "Images/github.png"; 
+var resume = new Image(); 
+resume.src = "Images/resume_logo.png"; 
+var mario1 = new Image(); 
+mario1.src = "Images/mario1.png"; 
+var mario2 = new Image(); 
+mario2.src = "Images/mario2.png"; 
+//////////////////////////////
+
+//Load Images
+github.onload = function (){
+    ctx.drawImage(github,200,50,50,50);
+}
+linkedin.onload = function (){
+    ctx.drawImage(linkedin,75,50,50,50);
+}
+resume.onload = function (){
+    ctx.drawImage(resume,350,50,40,55);
+}
+mario1.onload = function (){
+    ctx.drawImage(mario1,mario_x,mario_y,40,55);
+}
+mario2.onload = function (){
+    ctx.drawImage(mario2,mario_x,mario_y,40,55);
+}
+
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-
-
-
-function add_linkedin() { 
-    var linkedin = new Image(); 
-    linkedin.src = "Images/Linkedin.png"; 
-    linkedin.onload = function (){
-        ctx.drawImage(linkedin,75,50,50,50);
-    }
-    
-}
-
 
 function keyDownHandler(e) {
     if(e.code  == "ArrowRight") {
@@ -53,41 +74,56 @@ function keyUpHandler(e) {
     }
 }
 
-function add_github() { 
-    var github = new Image(); 
-    github.src = "Images/github.png"; 
-    github.onload = function (){
+function redraw_All() { 
+ 
+        ctx.drawImage(linkedin,75,50,50,50);
         ctx.drawImage(github,200,50,50,50);
-    }
-}
-    function add_resume() { 
-        var resume = new Image(); 
-        resume.src = "Images/resume_logo.png"; 
-        resume.onload = function (){
-            ctx.drawImage(resume,350,50,40,55);
-        }
+        ctx.drawImage(resume,350,50,40,55);
 }
 
-function add_mario(x, y) { 
-    var mario1 = new Image(); 
-    mario1.src = "Images/mario1.png"; 
 
-    var mario2 = new Image(); 
-    mario2.src = "Images/mario2.png"; 
-    mario1.onload = function (){
+
+function add_mario() { 
+    
+    
+
+    if (draw_mario1 === true){
         ctx.drawImage(mario1,mario_x,mario_y,40,55);
+        
     }
+    else{
+        ctx.drawImage(mario2,mario_x,mario_y,40,55);
+       
+    }
+
+    
+    if (rightPressed || leftPressed)
+    {
+        if (x <= 10)
+
+        {
+            x++;
+        }
+        else{
+            console.log(x);
+            x = 0;
+            draw_mario1 = !draw_mario1;
+            
+        }
+    }
+   
+    
+    
 }
 
 function draw ()
 {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    add_linkedin();
-    add_github();
-    add_resume();
+    
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    redraw_All();
     add_mario();
 
-    if(rightPressed && mario_x < canvas.width-20) {
+    if(rightPressed && mario_x < canvas.width-40) {
         mario_x += 7;
     }
     else if(leftPressed && mario_x > 0) {
@@ -97,8 +133,10 @@ function draw ()
         mario_y -= 50;
     }
 
- 
+    
     requestAnimationFrame(draw);
+    
+
 }
 
 draw();
